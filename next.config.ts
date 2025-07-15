5e325501b54254d939620adef4a6105e6fbd203b
ry0218 <ry0218@naver.com>
2025-07-15 13:46:24 +0900

    Feat: next config 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  
+  images: {
+    unoptimized: true,
+    domains: ["cdn.chalpu.com"],
+  },
 };
 
 export default nextConfig;
